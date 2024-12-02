f7f65eeea37be34b1053ab436e30090378aa1d84
Tushar Totla <tushartotla12@gmail.com>
2024-12-02 21:35:31 +0530

    multiple image uplaod

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: './.env.local' });
+// require('dotenv').config({ path: './.env.local' });
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
